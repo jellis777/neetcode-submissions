79c61cc4b75bf24c3d3703079d82d182819a40c4
Jeff Ellis <jellis777@gmail.com>
2026-04-20 09:24:02 -0500

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.js
@@ -0,0 +1,32 @@
+class Solution {
+    encode(strs) {
+        let res = ""; 
+        for (let str of strs) {
+            res += str.length + "#" + str; 
+        }
+        return res; 
+    }
+
+    decode(str) {
+        let res = []; 
+        let i = 0; 
+
+        while (i < str.length) {
+            let j = i; 
+            while (str[j] !== "#") {
+                j++; 
+            }
+
+            let len = Number(str.slice(i, j)); 
+            let word = str.slice(j + 1, j + 1 + len);
+            res.push(word);
+
+            i = j + 1 + len; 
+        }
+
+        return res; 
+    }
+
+
+    
+}
