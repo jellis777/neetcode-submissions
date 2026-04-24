bc9d7d29a866150f9987b9156832cebe293ff20c
Jeff Ellis <jellis777@gmail.com>
2026-04-24 13:29:39 -0500

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let left = 1;
+        let right = Math.max(...piles); 
+
+        while (left < right) {
+            let mid = Math.floor((left + right) / 2); 
+
+            let hours = 0;
+
+            for (let pile of piles) {
+                hours += Math.ceil(pile/mid);
+            }
+
+            if (hours <= h) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left; 
+    }
+}
