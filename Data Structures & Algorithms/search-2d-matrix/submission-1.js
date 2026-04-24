8ba3165068dddd088cfdc4c1c90ab4fc94e5f5cb
Jeff Ellis <jellis777@gmail.com>
2026-04-24 12:11:19 -0500

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        const rows = matrix.length;
+        const cols = matrix[0].length;
+
+        let left = 0;
+        let right = rows * cols - 1;
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+
+            let row = Math.floor(mid/cols); 
+            let col = mid % cols; 
+
+            if (matrix[row][col] === target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false; 
+    }
+}
