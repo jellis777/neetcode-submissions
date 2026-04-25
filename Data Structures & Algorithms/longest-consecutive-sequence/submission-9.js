5374c6e393cdddcb6752ef3af0fac3e70dc1409a
Jeff Ellis <jellis777@gmail.com>
2026-04-25 10:36:21 -0500

    Add: longest-consecutive-sequence - submission-9

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-9.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const set = new Set(nums); 
+        let longest = 0;
+
+        for (let num of nums) {
+            if (!set.has(num - 1)) {
+                let length = 1;
+                while (set.has(num + length)) {
+                    length++; 
+                }
+
+                longest = Math.max(longest, length); 
+            }
+        }
+
+        return longest; 
+    }
+}
