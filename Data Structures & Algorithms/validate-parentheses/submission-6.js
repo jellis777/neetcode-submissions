b6c35982001cbebe97c44523d9a31b116f63c07b
Jeff Ellis <jellis777@gmail.com>
2026-04-27 13:00:27 -0500

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = []; 
+        const closedToOpen = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '['
+        }
+
+        for (let char of s) {
+            if (closedToOpen[char]) {
+                if (stack.length > 0 && stack[stack.length -1] === closedToOpen[char]) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(char);
+            }
+        }
+
+        return stack.length === 0; 
+    }
+}
