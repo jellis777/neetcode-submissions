9247c801db59676854f9a57317efc3a1361fb0c3
Jeff Ellis <jellis777@gmail.com>
2026-04-28 15:32:51 -0500

    Add: longest-substring-without-duplicates - submission-19

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-19.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        const set = new Set(); 
+        let left = 0;
+        let maxLen = 0;
+
+        for (let right = 0; right < s.length; right++) {
+            while (set.has(s[right])) {
+                set.delete(s[left]);
+                left++
+            }
+
+            set.add(s[right]); 
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen; 
+
+    }
+}
+
