04fd9822bbeedf3aeef4e8f19c9ffb3fa08a732c
Jeff Ellis <jellis777@gmail.com>
2026-04-28 10:17:34 -0500

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.js
@@ -0,0 +1,54 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+
+    // DFS
+    canFinish(numCourses, prerequisites) {
+        const graph = {}; 
+
+        for (let i = 0; i < numCourses; i++) {
+            graph[i] = []
+        }
+
+        for (let [course, prereq] of prerequisites) {
+            graph[course].push(prereq);
+        }
+
+        const visiting = new Set();
+        const visited = new Set();
+
+        function dfs(course) {
+            if (visiting.has(course)) {
+                return false;
+            }
+
+            if (visited.has(course)) {
+                return true;
+            }
+
+            visiting.add(course); 
+
+            for (let prereq of graph[course]) {
+                if (!dfs(prereq)) {
+                    return false;
+                }
+            }
+
+            visiting.delete(course);
+            visited.add(course); 
+
+            return true;
+        }
+
+        for (let course = 0; course < numCourses; course++) {
+            if (!dfs(course)) {
+                return false;
+            }
+        }
+
+        return true; 
+    }
+}
