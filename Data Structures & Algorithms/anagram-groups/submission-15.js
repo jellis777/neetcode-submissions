16faf1d1324e66acdc6f39dd302b52fcbca37aab
Jeff Ellis <jellis777@gmail.com>
2026-04-28 14:38:05 -0500

    Add: anagram-groups - submission-15

---
+++ Data Structures & Algorithms/anagram-groups/submission-15.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const res = {};
+
+        for (let str of strs) {
+            const count = new Array(26).fill(0);
+
+            for (let char of str) {
+                count[char.charCodeAt(0) - 97]++;
+            }
+
+            const key = count.join(",");
+
+            if (!res[key]) {
+                res[key] = [];
+            } 
+             res[key].push(str);
+            
+        }
+
+        return Object.values(res); 
+    }
+}
