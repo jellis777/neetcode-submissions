b4ddce53965c8dd3eacb825abc371a0380f8e539
Jeff Ellis <jellis777@gmail.com>
2026-04-29 12:30:55 -0500

    Add: rotting-fruit - submission-6

---
+++ Data Structures & Algorithms/rotting-fruit/submission-6.js
@@ -0,0 +1,55 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    orangesRotting(grid) {
+        const rows = grid.length;
+        const cols = grid[0].length;
+
+        const queue = [];
+        let fresh = 0;
+        let minutes = 0;
+
+        const directions = [
+            [1, 0],
+            [-1, 0],
+            [0, 1],
+            [0, -1],
+        ];
+
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                if (grid[r][c] === 1) {
+                    fresh++;
+                } else if (grid[r][c] === 2){
+                    queue.push([r, c]);
+                }
+            }
+        }
+
+        let head = 0;
+        while (head < queue.length && fresh > 0) {
+            const levelSize = queue.length - head;
+
+            for (let i = 0; i < levelSize; i++) {
+                const [row, col] = queue[head++];
+
+                for (let [dr, dc] of directions) {
+                    const nr = row + dr;
+                    const nc = col + dc;
+
+                    if (nr >= 0 && nc >= 0 && nr < rows && nc < cols && grid[nr][nc] === 1) {
+                        queue.push([nr, nc]);
+                        grid[nr][nc] = 2;
+                        fresh--;
+                    }
+                }
+            }
+
+            minutes++; 
+        }
+
+        return fresh === 0 ? minutes : -1
+    }
+}
