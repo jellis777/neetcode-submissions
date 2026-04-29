fec7fe8abb4be9557288c36fab9a36fe1979e893
Jeff Ellis <jellis777@gmail.com>
2026-04-28 21:36:13 -0500

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        const count = {}; 
+        let left = 0
+        let maxLen = 0;
+        let maxFreq = 0;
+
+        for (let right = 0; right < s.length; right++) {
+            count[s[right]] = (count[s[right]] || 0) + 1;
+
+            maxFreq = Math.max(maxFreq, count[s[right]]);
+
+            while ((right - left + 1) - maxFreq > k) {
+                count[s[left]]--;
+                left++; 
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1); 
+        }
+
+        return maxLen; 
+    }
+}
