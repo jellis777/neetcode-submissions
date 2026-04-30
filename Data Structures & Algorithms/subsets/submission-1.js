9fd015f7e68f2707379b42c7155747fa7799bc7d
Jeff Ellis <jellis777@gmail.com>
2026-04-30 09:49:13 -0500

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums) {
+        const result = []; 
+
+        function backtrack(start, path) {
+            result.push([...path]);
+
+            for (let i = start; i < nums.length; i++) {
+                path.push(nums[i]);
+
+                backtrack(i + 1, path);
+
+                path.pop();
+            }
+        }
+
+        backtrack(0, []);
+        return result; 
+    }
+}
