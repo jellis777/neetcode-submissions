37af14d0496bf8192e2ccf5359148c0d25c93abc
Jeff Ellis <jellis777@gmail.com>
2026-05-01 09:25:50 -0500

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p, q) {
+        let queue = [[p, q]]; 
+
+        while (queue.length > 0) {
+            let [a, b] = queue.shift(); 
+
+            if (!a && !b) continue; 
+            if (!a || !b || a.val !== b.val) {
+                return false;
+            }
+
+            queue.push([a.left, b.left]);
+            queue.push([a.right, b.right]); 
+        }
+
+        return true; 
+    }
+}
