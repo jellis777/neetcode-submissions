a82e625a29c52fb0782c84cd38fd7e78a69bb0b2
Jeff Ellis <jellis777@gmail.com>
2026-05-01 12:50:19 -0500

    Add: count-number-of-islands - submission-30

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-30.js
@@ -0,0 +1,49 @@
+class Solution {
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+    numIslands(grid) {
+        const rows = grid.length;
+        const cols = grid[0].length;
+        let islands = 0;
+
+        let directions = [
+            [1, 0],
+            [-1, 0],
+            [0, 1],
+            [0, -1],
+        ];
+
+        function bfs(r, c) {
+            let queue = [[r, c]];
+            let head = 0;
+
+            grid[r][c] = "0";
+
+            while (head < queue.length) {
+                const [row, col] = queue[head++];
+
+                for (let [dr, dc] of directions) {
+                    const nr = row + dr;
+                    const nc = col + dc;
+
+                    if (nr >= 0 && nc >= 0 && nr < rows && nc < cols && grid[nr][nc] === "1") {
+                        queue.push([nr, nc]);
+                        grid[nr][nc] = "0";
+                    }
+                }
+            }
+        }
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                if (grid[r][c] === "1") {
+                    islands++;
+                    bfs(r, c);
+                }
+            }
+        }
+
+        return islands;
+    }
+}
