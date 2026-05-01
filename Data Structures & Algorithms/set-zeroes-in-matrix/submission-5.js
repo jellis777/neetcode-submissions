e9d1229453d0c284bdca4002afac95ac5712521d
Jeff Ellis <jellis777@gmail.com>
2026-05-01 12:18:43 -0500

    Add: set-zeroes-in-matrix - submission-5

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-5.js
@@ -0,0 +1,57 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {void}
+     */
+    setZeroes(matrix) {
+        const rows = matrix.length;
+        const cols = matrix[0].length;
+
+        let firstRowZero = false;
+        let firstColZero = false;
+
+        // check if first row has a zero
+        for (let c = 0; c < cols; c++) {
+            if (matrix[0][c] === 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        for (let r = 0; r < rows; r++) {
+            if (matrix[r][0] === 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        for (let r = 1; r < rows; r++) {
+            for (let c = 1; c < cols; c++) {
+                if (matrix[r][c] === 0) {
+                    matrix[r][0] = 0;
+                    matrix[0][c] = 0;
+                }
+            }
+        }
+
+        for (let r = 1; r < rows; r++) {
+            for (let c = 1; c < cols; c++) {
+                if (matrix[r][0] === 0 || matrix[0][c] === 0) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+
+        if (firstRowZero) {
+            for (let c = 0; c < cols; c++) {
+                matrix[0][c] = 0;
+            }
+        }
+
+        if (firstColZero) {
+            for (let r = 0; r < rows; r++) {
+                matrix[r][0] = 0;
+            }
+        }
+    }
+}
