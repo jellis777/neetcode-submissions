ecd9660b0b54e73ed01971839fb6a06c2ce26b68
Jeff Ellis <jellis777@gmail.com>
2026-05-01 07:56:41 -0500

    Add: invert-a-binary-tree - submission-6

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-6.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+
+    // DFS(Recursive) --- At every node: swap left and right children
+    // Pre-order DFS  ----- Node -> left -> right;
+    invertTree(root) {
+        if (!root) return null;
+
+        let queue = [root];
+
+        while (queue.length) {
+            const node = queue.shift();
+
+            [node.left, node.right] = [node.right, node.left];
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        return root;
+    }
+}
