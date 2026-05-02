4b28d1c0c047fc00db5af18a14db263ca06c925a
Jeff Ellis <jellis777@gmail.com>
2026-05-02 12:26:56 -0500

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortArray(nums) {
+        this.quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    quickSort(nums, left, right) {
+        if (left >= right) return;
+
+        const pivotIndex = this.partition(nums, left, right);
+
+        this.quickSort(nums, left, pivotIndex - 1);
+        this.quickSort(nums, pivotIndex + 1, right);
+    }
+
+    partition(nums, left, right) {
+        const pivot = nums[right];
+        let i = left;
+
+        for (let j = left; j < right; j++) {
+            if (nums[j] < pivot) {
+                [nums[i], nums[j]] = [nums[j], nums[i]];
+                i++;
+            }
+        }
+
+        [nums[i], nums[right]] = [nums[right], nums[i]];
+        return i; 
+    }
+}
