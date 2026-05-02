a715b2f94240fa0a09392130538c65e3610e731e
Jeff Ellis <jellis777@gmail.com>
2026-05-02 09:00:16 -0500

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    isHappy(n) {
+        const seen = new Set();
+
+        while (n !== 1 && !seen.has(n)) {
+            seen.add(n);
+            n = this.sumOfSquares(n);
+        }
+
+        return n === 1;
+    }
+
+    sumOfSquares(n) {
+        let sum = 0;
+
+        while (n > 0) {
+            const digit = n % 10;
+            sum += digit * digit;
+            n = Math.floor(n / 10);
+        }
+
+        return sum;
+    }
+}
