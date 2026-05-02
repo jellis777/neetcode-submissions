0f75f8055c3ee5ba6eb5391b1ee732424a442413
Jeff Ellis <jellis777@gmail.com>
2026-05-02 17:44:43 -0500

    Add: buy-two-chocolates - submission-1

---
+++ Data Structures & Algorithms/buy-two-chocolates/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @param {number} money
+     * @return {number}
+     */
+    buyChoco(prices, money) {
+        let min1 = Infinity;
+        let min2 = Infinity;
+
+        for (let price of prices) {
+            if (price < min1) {
+                min2 = min1;
+                min1 = price;
+            } else if (price < min2) {
+                min2 = price;
+            }
+        }
+
+        let total = min1 + min2;
+
+        if (total > money) return money;
+
+        return money - total; 
+    }
+}
