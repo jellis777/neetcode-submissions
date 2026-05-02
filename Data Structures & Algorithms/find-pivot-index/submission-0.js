eee2ac2db8f6ad5c21c7354f58352e9202e608d6
Jeff Ellis <jellis777@gmail.com>
2026-05-02 18:01:11 -0500

    Add: find-pivot-index - submission-0

---
+++ Data Structures & Algorithms/find-pivot-index/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    pivotIndex(nums) {
+        let total = nums.reduce((sum, n) => sum + n, 0);
+        let left = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+            let right = total - left - nums[i]; 
+
+            if (left === right) {
+                return i;
+            }
+
+            left += nums[i]; 
+        }
+
+        return -1; 
+    }
+}
