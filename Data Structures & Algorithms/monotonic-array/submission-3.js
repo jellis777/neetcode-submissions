1265ec3813734ddbcda5de9048b1814a4b46bcc4
Jeff Ellis <jellis777@gmail.com>
2026-05-02 21:26:22 -0500

    Add: monotonic-array - submission-3

---
+++ Data Structures & Algorithms/monotonic-array/submission-3.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    isMonotonic(nums) {
+        let isIncreasing = true;
+        let isDecreasing = true;
+
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                isDecreasing = false;
+            }
+
+            if (nums[i] < nums[i - 1]) {
+                isIncreasing = false; 
+            }
+        }
+
+        return isIncreasing || isDecreasing; 
+    }
+}
