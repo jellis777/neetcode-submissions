c20f0242bf1426d16142580d68281444b80741b0
Jeff Ellis <jellis777@gmail.com>
2026-05-04 06:55:50 -0500

    Add: depth-of-binary-tree - submission-15

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-15.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+
+    // BFS  Level 1 -> root; Level 2 -> children; Level 3 -> grandchildren;
+    maxDepth(root) {
+        if (!root) return 0;
+
+        let queue = [root];
+        let depth = 0; 
+
+        while (queue.length) {
+            let levelSize = queue.length;
+
+            for (let i = 0; i < levelSize; i++) {
+                let node = queue.shift();
+
+                if (node.left) queue.push(node.left);
+                if (node.right) queue.push(node.right);
+            }
+
+            depth++;
+        }
+
+        return depth; 
+    }
+}
