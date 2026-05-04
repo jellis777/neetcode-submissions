c2178b6b1b1f7e52daf7b319db12ce39a320e36e
Jeff Ellis <jellis777@gmail.com>
2026-05-04 09:18:02 -0500

    Add: permutation-string - submission-30

---
+++ Data Structures & Algorithms/permutation-string/submission-30.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        if (s1.length > s2.length) return false;
+
+        const count1 = new Array(26).fill(0);
+        const count2 = new Array(26).fill(0);
+
+        for (let char of s1) {
+            count1[char.charCodeAt(0) - 97]++;
+        }
+
+        let left = 0; 
+        for (let right = 0; right < s2.length; right++) {
+            let char = s2[right];
+            count2[char.charCodeAt(0) - 97]++;
+
+            if (right - left + 1 > s1.length) {
+                count2[s2[left].charCodeAt(0) - 97]--;
+                left++;
+            }
+
+            if (this.arrayEquals(count1, count2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    arrayEquals(a, b) {
+        for (let i = 0; i < 26; i++) {
+            if (a[i] !== b[i]) return false;
+        }
+
+        return true;
+    }
+}
