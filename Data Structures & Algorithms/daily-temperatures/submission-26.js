077073e686d2798b3a4d640b7dec0c9c6172e51d
Jeff Ellis <jellis777@gmail.com>
2026-05-04 07:55:41 -0500

    Add: daily-temperatures - submission-26

---
+++ Data Structures & Algorithms/daily-temperatures/submission-26.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const output = new Array(temperatures.length).fill(0);
+        const stack = [];
+
+        for (let i = 0; i < temperatures.length; i++) {
+            while (temperatures[i] > temperatures[stack[stack.length - 1]]) {
+                let prevIndex = stack.pop();
+                output[prevIndex] = i - prevIndex;
+            }
+
+            stack.push(i);
+        }
+
+        return output;
+    }
+}
