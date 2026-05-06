01a6ef1164c463bdcbf762e7e7a7b181b1bbf4bb
Jeff Ellis <jellis777@gmail.com>
2026-05-06 08:00:22 -0500

    Add: unique-email-addresses - submission-0

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-0.js
@@ -0,0 +1,44 @@
+class Solution {
+   /*
+    I/O: 
+    -Input: emails: string[];
+    -Output: number of unique email addresses
+
+    RULES:
+    -Split at '@'
+    -In local name:
+        -ignore '.'
+        -ignore everything after first '+'
+    - Domain stays unchanged
+
+    Edge cases:
+    - local has dots only between characters
+    - local has +
+    -local has both dot and plus
+    -same local but different domain -> different email
+
+    Optimal:
+    - Normalize each email
+    - Add to Set
+    - Return Set size
+   */
+
+
+
+
+    numUniqueEmails(emails) {
+        const unique = new Set(); 
+
+        for (const email of emails) {
+            const [local, domain] = email.split("@"); 
+
+            const cleanLocal = local
+                .split("+")[0]
+                .replaceAll(".", "");
+
+            unique.add(cleanLocal + '@' + domain)
+        }
+
+        return unique.size
+    }
+}
