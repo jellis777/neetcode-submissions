f118dc7753d9850b9aeb6a2ea67fe567c469aa74
Jeff Ellis <jellis777@gmail.com>
2026-05-05 19:40:10 -0500

    Add: max-water-container - submission-9

---
+++ Data Structures & Algorithms/max-water-container/submission-9.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let maxArea = 0;
+        let left = 0;
+        let right = heights.length - 1;
+
+        while (left < right) {
+            let width = right - left;
+            let h = Math.min(heights[left], heights[right]);
+            let area = h * width;
+
+            maxArea = Math.max(maxArea, area);
+
+            if (heights[right] > heights[left]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
