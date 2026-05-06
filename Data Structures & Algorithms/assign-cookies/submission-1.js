95af1a21a5112236a17f6742bb3379887e4ccb07
Jeff Ellis <jellis777@gmail.com>
2026-05-05 22:37:20 -0500

    Add: assign-cookies - submission-1

---
+++ Data Structures & Algorithms/assign-cookies/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} g
+     * @param {number[]} s
+     * @return {number}
+     
+     RULES: - Each child is given at most one cookie
+            -  s[j] >= g[i]
+    
+    RETURN amount of children that are content; 
+    - Map {}
+
+     */
+    findContentChildren(g, s) {
+        g.sort((a, b) => a - b);
+        s.sort((a, b) => a - b);
+
+        let child = 0;
+        let cookie = 0;
+
+        while (child < g.length && cookie < s.length) {
+            if (s[cookie] >= g[child]) {
+                child++;
+            }
+
+            cookie++;
+        }
+
+        return child; 
+    }
+}
