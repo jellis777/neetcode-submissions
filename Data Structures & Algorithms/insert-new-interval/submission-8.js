8b8c7223ed5d0c7ed7b576cf5ff622ede22b40ce
Jeff Ellis <jellis777@gmail.com>
2026-05-08 13:16:43 -0500

    Add: insert-new-interval - submission-8

---
+++ Data Structures & Algorithms/insert-new-interval/submission-8.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @param {number[]} newInterval
+     * @return {number[][]}
+     */
+    insert(intervals, newInterval) {
+        const result = [];
+
+        for (let i = 0; i < intervals.length; i++) {
+            const [start, end] = intervals[i];
+
+            if (end < newInterval[0]) {
+                result.push(intervals[i]);
+            } else if (start > newInterval[1]) {
+                result.push(newInterval);
+
+                for (let j = i; j < intervals.length; j++) {
+                    result.push(intervals[j]);
+                }
+
+                return result;
+            } else {
+                newInterval[0] = Math.min(newInterval[0], start);
+                newInterval[1] = Math.max(newInterval[1], end);
+            }
+        }
+        result.push(newInterval);
+        return result; 
+    }
+}
