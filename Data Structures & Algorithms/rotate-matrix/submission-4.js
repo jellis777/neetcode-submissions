29d8da1874462a16466d786efa90a05f43bbdf8a
Jeff Ellis <jellis777@gmail.com>
2026-05-11 18:03:16 -0500

    Add: rotate-matrix - submission-4

---
+++ Data Structures & Algorithms/rotate-matrix/submission-4.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {void}
+     */
+    rotate(matrix) {
+        const n = matrix.length;
+
+        for (let r = 0; r < n; r++) {
+            for (let c = r + 1; c < n; c++) {
+                [matrix[r][c], matrix[c][r]] = [matrix[c][r], matrix[r][c]]
+            }
+        }
+
+        for (let r = 0; r < n; r++) {
+            matrix[r].reverse(); 
+        }
+    }
+}
