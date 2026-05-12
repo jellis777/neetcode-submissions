b3ca88e0fc34f84ae4ad44bc8772d79779485477
Jeff Ellis <jellis777@gmail.com>
2026-05-12 09:15:40 -0500

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permute(nums) {
+        const result = [];
+        const path = []; 
+        const used = new Set(); 
+
+        function backtrack() {
+            if (path.length === nums.length) {
+                result.push([...path]);
+                return;
+            }
+
+            for (let num of nums) {
+                if (used.has(num)) continue; 
+
+                path.push(num);
+                used.add(num); 
+
+                backtrack(); 
+
+                path.pop();
+                used.delete(num); 
+            }
+        }
+
+        backtrack();
+        return result; 
+    }
+}
