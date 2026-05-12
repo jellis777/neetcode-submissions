2af3481bd519e0cad4c5ec852a2792ffa4b778ea
Jeff Ellis <jellis777@gmail.com>
2026-05-11 22:10:51 -0500

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    jump(nums) {
+        let farthest = 0; 
+        let currentEnd = 0;
+        let jumps = 0;
+
+        for (let i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i === currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps; 
+    }
+}
