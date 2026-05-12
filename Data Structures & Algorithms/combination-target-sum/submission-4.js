078539b4b673749f560ed791d0369a281bb4a5de
Jeff Ellis <jellis777@gmail.com>
2026-05-11 20:32:21 -0500

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        const result = [];
+
+        function backtrack(start, path, sum) {
+            if (sum === target) {
+                result.push([...path])
+                return;
+            }
+
+            if (sum > target) {
+                return ;
+            }
+
+            for (let i = start; i < nums.length; i++) {
+                path.push(nums[i]) 
+
+                backtrack(i, path, sum + nums[i]);
+
+                path.pop(); 
+            }
+        }
+
+        backtrack(0, [], 0); 
+        return result; 
+    }
+}
