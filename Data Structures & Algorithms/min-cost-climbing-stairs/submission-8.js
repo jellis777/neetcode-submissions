827036990b9885f91372d278e5dd56f32ecd9c11
Jeff Ellis <jellis777@gmail.com>
2026-05-12 07:26:43 -0500

    Add: min-cost-climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-8.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost) {
+        const n = cost.length;
+        const dp = new Array(n);
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (let i = 2; i < cost.length; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+        }
+
+        return Math.min(dp[n - 1], dp[n - 2]);
+    }
+}
