585531b949830a3179d7a6e2ecb7af436f84ee81
Jeff Ellis <jellis777@gmail.com>
2026-05-14 15:26:22 -0500

    Add: buy-and-sell-crypto - submission-18

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-18.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let l = 0; 
+        let r = 1;
+        let maxProfit = 0;
+
+        while (r < prices.length) {
+            if (prices[l] < prices[r]) {
+                let profit = prices[r] - prices[l]; 
+                maxProfit = Math.max(maxProfit, profit); 
+            } else {
+                l = r; 
+            }
+
+            r++; 
+        }
+
+        return maxProfit; 
+    }
+}
