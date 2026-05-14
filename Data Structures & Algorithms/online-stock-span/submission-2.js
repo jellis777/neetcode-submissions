d518e0db0de8815854a631ebf3b1c409d5be9459
Jeff Ellis <jellis777@gmail.com>
2026-05-14 14:57:33 -0500

    Add: online-stock-span - submission-2

---
+++ Data Structures & Algorithms/online-stock-span/submission-2.js
@@ -0,0 +1,31 @@
+class StockSpanner {
+    constructor() {
+        this.stack = []; 
+    }
+
+    /**
+     * @param {number} price
+     * @return {number}
+     */
+    next(price) {
+        let span = 1;
+
+        while (
+            this.stack.length &&
+            this.stack[this.stack.length - 1][0] <= price
+        ) {
+            const [prevPrice, prevSpan] = this.stack.pop(); 
+            span += prevSpan
+        }
+
+        this.stack.push([price, span]);
+
+        return span; 
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */
