dc5d1555bc750dbecceab24e6126d80df049f07c
Jeff Ellis <jellis777@gmail.com>
2026-05-15 09:44:41 -0500

    Add: binary-tree-preorder-traversal - submission-3

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-3.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    preorderTraversal(root) {
+        if (!root) return []
+
+        let result = []; 
+        let stack = [root];
+
+        while (stack.length) {
+
+            const node = stack.pop(); 
+
+            result.push(node.val)
+
+            if (node.right) stack.push(node.right);
+            if (node.left) stack.push(node.left);
+        }
+
+        return result; 
+    }
+}
