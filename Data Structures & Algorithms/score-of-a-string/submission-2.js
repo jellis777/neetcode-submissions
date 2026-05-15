a11575e6096a6ace33c2bbf7b7208ff07f9f1060
Jeff Ellis <jellis777@gmail.com>
2026-05-15 11:48:22 -0500

    Add: score-of-a-string - submission-2

---
+++ Data Structures & Algorithms/score-of-a-string/submission-2.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    scoreOfString(s) {
+        let score = 0;
+
+        for (let i = 0; i < s.length -1; i++) {
+            let curr = s.charCodeAt(i);
+            let next = s.charCodeAt(i + 1);
+
+            score += Math.abs(curr - next);
+        }
+
+        return score; 
+    }
+}
