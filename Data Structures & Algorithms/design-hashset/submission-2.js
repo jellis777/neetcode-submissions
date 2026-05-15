7ea2d040c9074517428d14bed5a52320a1ce42f9
Jeff Ellis <jellis777@gmail.com>
2026-05-15 11:00:59 -0500

    Add: design-hashset - submission-2

---
+++ Data Structures & Algorithms/design-hashset/submission-2.js
@@ -0,0 +1,54 @@
+class MyHashSet {
+    constructor() {
+        this.size = 1000;
+        this.buckets = Array.from(
+            {length: this.size}, 
+            () => []
+        );
+    }
+
+    hash(key) {
+        return key % this.size;
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    add(key) {
+        const index = this.hash(key); 
+
+        if (!this.buckets[index].includes(key)) {
+            this.buckets[index].push(key);
+        }
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        const index = this.hash(key); 
+
+        this.buckets[index] = 
+        this.buckets[index].filter(num => num !== key);
+    }
+
+    /**
+     * @param {number} key
+     * @return {boolean}
+     */
+    contains(key) {
+        const index = this.hash(key); 
+
+        return this.buckets[index].includes(key); 
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
