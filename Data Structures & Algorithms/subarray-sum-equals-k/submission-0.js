434818342149966a6a5473e3c4d0152dae635528
Jeff Ellis <jellis777@gmail.com>
2026-05-14 22:17:08 -0500

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    subarraySum(nums, k) {
+        let count = 0;
+        let sum = 0;
+
+        const prefixCount = new Map();
+        prefixCount.set(0, 1); 
+
+        for (const num of nums) {
+            sum += num;
+
+            const needed = sum - k;
+
+            if (prefixCount.has(needed)) {
+                count += prefixCount.get(needed);
+            }
+
+            prefixCount.set(sum, (prefixCount.get(sum) || 0) + 1); 
+        }
+
+        return count; 
+    }
+}
