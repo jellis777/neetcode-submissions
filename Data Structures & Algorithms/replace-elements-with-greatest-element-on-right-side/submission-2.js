76a95700d4bc7c438bfd4ddc9f83cb5acb59cd20
Jeff Ellis <jellis777@gmail.com>
2026-05-15 12:04:37 -0500

    Add: replace-elements-with-greatest-element-on-right-side - submission-2

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+        let rightMax = -1;
+
+        for (let i = arr.length - 1; i >= 0; i--) {
+            let curr = arr[i]; 
+
+            arr[i] = rightMax;
+
+            rightMax = Math.max(rightMax, curr); 
+        }
+
+        return arr; 
+    }
+}
