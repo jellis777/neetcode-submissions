fd369399cea58f0ddf8d187073459ce938319000
Jeff Ellis <jellis777@gmail.com>
2026-05-14 22:50:36 -0500

    Add: valid-tree - submission-8

---
+++ Data Structures & Algorithms/valid-tree/submission-8.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        if (edges.length !== n - 1) return false;
+
+        const adj = Array.from( {length: n}, () => []);
+
+        for (let [a, b] of edges) {
+            adj[a].push(b);
+            adj[b].push(a);
+        }
+
+        const visited = new Set();
+
+        function dfs(node) {
+            if (visited.has(node)) return; 
+
+            visited.add(node);
+
+            for (let neigh of adj[node]) {
+                dfs(neigh); 
+            }
+        }
+
+        dfs(0); 
+        return n === visited.size; 
+    }
+}
