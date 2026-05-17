916c7df9c769a3744e166dc0e171fe2a5439a8b4
Jeff Ellis <jellis777@gmail.com>
2026-05-17 15:47:51 -0500

    Add: open-the-lock - submission-7

---
+++ Data Structures & Algorithms/open-the-lock/submission-7.js
@@ -0,0 +1,56 @@
+class Solution {
+    /**
+     * @param {string[]} deadends
+     * @param {string} target
+     * @return {number}
+     */
+    openLock(deadends, target) {
+        const deads = new Set(deadends);
+
+        if (deads.has("0000")) return -1;
+        if (target === "0000") return 0;
+
+        const queue = ["0000"];
+        const visited = new Set(["0000"]);
+        let head = 0;
+        let turns = 0;
+
+        while (head < queue.length) {
+            const levelSize = queue.length - head;
+
+            for (let i = 0; i < levelSize; i++) {
+                const code = queue[head++];
+
+                if (code === target) return turns;
+
+                for (let next of this.getNeighbors(code)) {
+                    if (!deads.has(next) && !visited.has(next)) {
+                        visited.add(next);
+                        queue.push(next)
+                    }
+                }
+            }
+
+            turns++;
+        }
+
+        return -1; 
+    }
+
+
+    getNeighbors(code) {
+        let result = []; 
+
+        for (let i = 0; i < 4; i++) {
+            let digit = Number(code[i]);
+
+            const up = (digit + 1) % 10;
+            const down = (digit + 9) % 10; 
+
+            result.push(code.slice(0, i) + up + code.slice(i + 1));
+            result.push(code.slice(0, i) + down + code.slice(i + 1)); 
+        }
+
+        return result; 
+    }
+}
