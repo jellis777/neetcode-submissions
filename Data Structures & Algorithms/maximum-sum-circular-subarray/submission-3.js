2b3f2ad39afde2c232f0d32b0f9d1b833ae7ee91
Jeff Ellis <jellis777@gmail.com>
2026-05-18 22:21:27 -0500

    Add: maximum-sum-circular-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-3.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubarraySumCircular(nums) {
+        let total = 0;
+
+        let currMax = 0; 
+        let maxSum = nums[0];
+
+        let currMin = 0; 
+        let minSum = nums[0]; 
+
+        for (let num of nums) {
+            total += num;
+
+
+            currMax = Math.max(num, currMax + num); 
+            maxSum = Math.max(currMax, maxSum);
+
+            currMin = Math.min(num, currMin + num); 
+            minSum = Math.min(currMin, minSum); 
+
+        }
+
+        if (maxSum < 0 ) return maxSum;
+
+        return Math.max(maxSum, total - minSum)
+    }
+}
