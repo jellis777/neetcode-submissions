55a020fe456d63751dcf907be89bd6d7fc548819
Jeff Ellis <jellis777@gmail.com>
2026-05-18 22:08:13 -0500

    Add: island-perimeter - submission-4

---
+++ Data Structures & Algorithms/island-perimeter/submission-4.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    islandPerimeter(grid) {
+        const rows = grid.length
+        const cols = grid[0].length;
+        let perimeter = 0;
+
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                if (grid[r][c] === 1) {
+                    perimeter += 4
+
+                    if (r > 0 && grid[r - 1][c] === 1) {
+                        perimeter -= 2
+                    }
+
+                    if (c > 0 && grid[r][c - 1] === 1) {
+                        perimeter -= 2;
+                    }
+                }
+            }
+        }
+
+        return perimeter; 
+    }
+}
