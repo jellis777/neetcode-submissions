b278a2e0c2c069bafe9343f83899eb150d0d5f96
Jeff Ellis <jellis777@gmail.com>
2026-05-21 15:01:41 -0500

    Add: wiggle-sort - submission-1

---
+++ Data Structures & Algorithms/wiggle-sort/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    wiggleSort(nums) {
+
+        for (let i = 0; i < nums.length; i++) {
+            if (
+            (i % 2 === 0 && nums[i] > nums[i + 1]) ||
+            (i % 2 === 1 && nums[i] < nums[i + 1])
+            ) {
+                [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]]
+            }
+        }
+
+        return nums; 
+    }
+}
