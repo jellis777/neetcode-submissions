ff71d74cb21258a5d0a68d8ff3e958ee5f95a91e
Jeff Ellis <jellis777@gmail.com>
2026-05-24 16:50:56 -0500

    Add: assign-cookies - submission-7

---
+++ Data Structures & Algorithms/assign-cookies/submission-7.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} g
+     * @param {number[]} s
+     * @return {number}
+     */
+    findContentChildren(g, s) {
+        g.sort((a, b) => a - b);
+        s.sort((a, b) => a - b);
+
+        let child = 0;
+        let cookie = 0;
+
+        while (child < g.length && cookie < s.length) {
+            if (g[child] <= s[cookie]) {
+                child++;
+            }
+            cookie++;
+        }
+        return child;
+    }
+}
