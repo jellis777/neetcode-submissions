d865905444af6b191dbfdbd701d45b24ebfa2bd9
Jeff Ellis <jellis777@gmail.com>
2026-05-23 21:16:34 -0500

    Add: next-greater-element-i - submission-2

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-2.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    nextGreaterElement(nums1, nums2) {
+        const stack = [];
+        const nextGreater = {};
+
+        for (let num of nums2) {
+            while (stack.length > 0 && num > stack[stack.length - 1]) {
+                const smaller = stack.pop();
+                nextGreater[smaller] = num;
+            }
+
+            stack.push(num)
+        }
+
+        return nums1.map((num) => nextGreater[num] ?? - 1); 
+    }
+}
