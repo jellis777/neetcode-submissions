3c9d58e0a5194a86f96ee5fe15a34c56eaac8c98
Jeff Ellis <jellis777@gmail.com>
2026-05-27 12:33:13 -0500

    Add: merge-sorted-array - submission-6

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-6.js
@@ -0,0 +1,55 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number} m
+     * @param {number[]} nums2
+     * @param {number} n
+     * @return {void} Do not return anything, modify nums1 in-place instead.
+     */
+    merge(nums1, m, nums2, n) {
+        let i = m - 1;
+        let j = n - 1;
+        let k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+    }
+}
+
+/**
+ CLARIFY: 
+    - INPUT: nums1 -> []int;  m -> int (# of nums1 to be merged); nums2 -> []int; n -> int (# of nums2 to be merged)
+    - OUTPUT: []int   ->  Modified in-place; sorted by ascending order; No return;
+    - CONSTRAINTS/ RULES: 
+            - Both nums1 and nums 2 are sorted
+            - m, n <= 200; 
+    - EDGE CASES: 
+            - nums1 = []; m = 0; n = 0; nums2 = []  -> return []; 
+            - Most edge cases are covered by the constraints; 
+
+- INITIAL APPROACH:
+    -   i = m - 1;
+    -   j = n - 1;
+    -   k = n + m - 1
+
+    while (j >= 0)
+    nums1[i]  > nums2[j] {nums1[i] swap with nums1[k] }  i--; k--;    
+
+        - Then nums1[k] = nums2[j]
+
+
+        nums1 = [10,20,20,40,0,0], m = 4, nums2 = [1,2], n = 2
+                   k                                  j
+                [0,0,10,20, 20,40]
+
+    Time: O(m + n)
+    Space: O(1)
+ */
