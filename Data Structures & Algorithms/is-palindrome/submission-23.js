6bbc9e72753810fd3409012cd22e4f5ab545d366
Jeff Ellis <jellis777@gmail.com>
2026-06-04 13:44:51 -0500

    Add: is-palindrome - submission-23

---
+++ Data Structures & Algorithms/is-palindrome/submission-23.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length - 1; 
+
+        while (left < right) {
+            while (left < right && !this.isAlphaNum(s[left])) {
+                left++;
+            }
+
+            while (left < right && !this.isAlphaNum(s[right])) {
+                right--;
+            }
+
+            if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+                return false; 
+            }
+            right--;
+            left++;
+        }
+
+        return true; 
+    }
+
+    isAlphaNum(char) {
+        return /^[a-z0-9]$/ig.test(char)
+    }
+}
