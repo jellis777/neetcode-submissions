5e592c45d8613e8b7ff0c6b06f037a4a2601ce5b
Jeff Ellis <jellis777@gmail.com>
2026-06-06 08:57:35 -0500

    Add: largest-number - submission-1

---
+++ Data Structures & Algorithms/largest-number/submission-1.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {string}
+     */
+    largestNumber(nums) {
+        const strs = nums.map(String); 
+
+        strs.sort((a, b) => (b + a).localeCompare(a + b));
+
+        const result = strs.join("")
+
+        return result[0] === "0" ? "0" : result; 
+    }
+}
+/**
+ INPUT: []int
+
+ OUTPUT: string  ->  largest possible number from rearranging nums in string format
+
+ CONSTRAINTS: 1 <= nums.length <= 100
+              0 <= nums[i] <= 1,000,000,000
+
+ EDGE CASES: [0, 0, 0]  -> "0"
+             [3] -> "3"
+             [3, 444, 50000, 1000] -> multiple varieties of digits  "5000010004443"
+
+ INITIAL APPROACH: - Compare first int then second; if tied with first int then compare second with first of other
+                   - Could do brute force 
+
+
+
+ */
