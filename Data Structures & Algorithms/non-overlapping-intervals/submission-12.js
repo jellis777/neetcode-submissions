560309005a3d6e7454ec7e086a67ab042018cdd3
Jeff Ellis <jellis777@gmail.com>
2026-06-07 17:00:26 -0500

    Add: non-overlapping-intervals - submission-12

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-12.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals) {
+        let removals = 0;
+        intervals.sort((a, b) => a[1] - b[1]);
+        let prevEnd = intervals[0][1];
+
+        for (let i = 1; i < intervals.length; i++) {
+            const [start, end] = intervals[i];
+
+            if (start < prevEnd) {
+                removals++;
+            } else {
+                prevEnd = end;
+            }
+        }
+
+        return removals;
+    }
+}
