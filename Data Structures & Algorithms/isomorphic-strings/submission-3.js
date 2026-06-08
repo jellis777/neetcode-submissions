6dc8181f1ee1f6ca96243f474a18209a71e9db92
Jeff Ellis <jellis777@gmail.com>
2026-06-08 08:44:59 -0500

    Add: isomorphic-strings - submission-3

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-3.js
@@ -0,0 +1,61 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isIsomorphic(s, t) {
+        const sToT = {};
+        const tToS = {};
+
+        for (let i = 0; i < s.length; i++) {
+            const sChar = s[i];
+            const tChar = t[i]; 
+
+            if (sToT[sChar] !== undefined && sToT[sChar] !== tChar) {
+                return false;
+            }
+
+            if (tToS[tChar] !== undefined && tToS[tChar] !== sChar) {
+                return false;
+            }
+
+            sToT[sChar] = tChar;
+            tToS[tChar] = sChar; 
+        }
+
+        return true; 
+    }
+}
+
+/**
+  INPUT: string  -> s
+         string  -> t 
+
+  OUTPUT: boolean -> True if both s and t are isomorphic (if the characters in s can be replaced to get t)
+
+  CONSTRAINTS: 0 <= s.length == t.length <= 50,000
+               s and t consists of any valid ascii character
+
+  EDGE CASES: s = ""  t = ""  return true
+              s = ""   t= "ab"  return false
+              s = "add" t="bee"  return true
+
+  INITIAL APPROACH: - Two hashmaps for mapping each character both ways from s -> t  and t -> s
+                            - sToTMap
+                            - tToSMap
+                    
+                    -Traverse over s 
+                        - If (sToTMap[char] !== tToSMap[char]  && 
+                                tToSMap[char] !== sToTMap[char]  )  {
+                                return false
+                                }
+                        - Insert into each map the character of the other string
+                    - return true
+
+                    Time: O(n);
+                    Space: O(n); 
+                        
+
+    
+ */
