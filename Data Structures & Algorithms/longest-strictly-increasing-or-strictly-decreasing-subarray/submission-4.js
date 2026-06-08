6a42ab954db68d2a16f3c16d875f7cffd90f32b9
Jeff Ellis <jellis777@gmail.com>
2026-06-08 09:49:12 -0500

    Add: longest-strictly-increasing-or-strictly-decreasing-subarray - submission-4

---
+++ Data Structures & Algorithms/longest-strictly-increasing-or-strictly-decreasing-subarray/submission-4.js
@@ -0,0 +1,57 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestMonotonicSubarray(nums) {
+        let maxLength = 1;
+        let incLength = 1;
+        let decLength = 1;
+
+        // strictly increasing
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                incLength++;
+                decLength = 1;
+            } else if (nums[i] < nums[i - 1]) {
+                decLength++;
+                incLength = 1;
+            } else {
+                decLength = 1;
+                incLength = 1;
+            }
+
+            maxLength = Math.max(maxLength, incLength, decLength);
+        }
+
+        return maxLength;
+    }
+}
+
+/**
+ INPUT: []int -> nums
+ OUTPUT: int  ->  length of the LONGEST subarray of nums (either strictly increasing or decreasing)
+
+ CONSTRAINTS:    1 <= nums.length <= 50;
+                 1 <= nums[i] <= 50;  
+
+ EDGE CASES:   - [1]     ->  1
+               - [1, 1, 4, 5, 6]  -> 4
+
+ INITIAL APPROACH:  - maxLength = 0
+                    - length = 1
+                    - Iterate at i = 1 < nums.length 
+                            If nums[i] >  nums[i - 1] 
+                                    - THEN  length++
+                            OTHERWISE:  length = 1; 
+                    - Iterate at i = 1 < nums.length 
+                            If nums[i] <  nums[i - 1] 
+                                    - THEN  length++
+                            OTHERWISE:  length = 1; 
+                     - maxLength = Math.max(maxLength, length); 
+
+                     return maxLength;
+
+                Time: O(n); 
+                Space: O(1)
+ */
