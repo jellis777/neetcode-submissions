1d9b7d6ac7140b150a1d1f4956e25a6ed2548f12
Jeff Ellis <jellis777@gmail.com>
2026-06-07 21:11:35 -0500

    Add: spiral-matrix - submission-24

---
+++ Data Structures & Algorithms/spiral-matrix/submission-24.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {number[]}
+     */
+    spiralOrder(matrix) {
+        let result = [];
+        let top = 0;
+        let left = 0;
+        let right = matrix[0].length - 1;
+        let bottom = matrix.length - 1;
+
+        while (top <= bottom && left <= right) {
+            for (let c = left; c <= right; c++) {
+                result.push(matrix[top][c]);
+            }
+            top++;
+
+            for (let r = top; r <= bottom; r++) {
+                result.push(matrix[r][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (let c = right; c >= left; c--) {
+                    result.push(matrix[bottom][c]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (let r = bottom; r >= top; r--) {
+                    result.push(matrix[r][left]);
+                }
+                left++;
+            }
+        }
+
+        return result; 
+    }
+}
