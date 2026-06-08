d4a5eb34bf708a56fd287afa21f2f9ae9c994be8
Jeff Ellis <jellis777@gmail.com>
2026-06-08 10:44:48 -0500

    Add: kth-distinct-string-in-an-array - submission-4

---
+++ Data Structures & Algorithms/kth-distinct-string-in-an-array/submission-4.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {string[]} arr
+     * @param {number} k
+     * @return {string}
+     */
+    kthDistinct(arr, k) {
+        const count = {}; 
+
+        for (let str of arr) {
+            count[str] = (count[str] || 0) + 1
+        }
+
+        const distinct = arr.filter((str) => count[str] === 1); 
+
+        return distinct[k - 1] ?? ""
+    }
+}
+
+/**
+ INPUT: []string -> arr
+        int  -> k
+
+ OUTPUT: string  -> the kth distinct string in arr
+
+ CONSTRAINTS: 1 <= k <= arr.length <= 1000
+              1 <= arr[i].length <= 5 
+              arr[i] consists of lowercase letters
+
+ EDGE CASES:  arr = ["a", "a", "b", "b"], k = 1   -> ""
+              arr = ["a", "b", "c"] k = 2  -> "b"
+
+ INITIAL APPROACH: 
+        - use a hashMap to keep count of strings
+        - Iterate over keys of hashMap to find counts of 1 
+                - return kth key
+                - else return ""
+
+        Time: O(n);
+        Space: O(n)
+ */
