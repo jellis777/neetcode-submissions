04b85941eff15ea6d28fe0cc202c419705099f66
Jeff Ellis <jellis777@gmail.com>
2026-06-09 12:02:28 -0500

    Add: special-array-with-x-elements-greater-than-or-equal-x - submission-2

---
+++ Data Structures & Algorithms/special-array-with-x-elements-greater-than-or-equal-x/submission-2.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    specialArray(nums) {
+        for (let x = 0; x <= nums.length; x++) {
+            let count = 0;
+
+            for (let num of nums) {
+                if (num >= x) {
+                    count++;
+                }
+            }
+
+            if (count === x) {
+                return x
+            }
+        }
+
+        return -1; 
+    }
+}
+
+/**
+ INPUT: []int
+ OUTPUT: int  
+
+ CONSTRAINTS: 1 <= nums.length <= 100
+              0 <= num[i] <= 1000
+
+ EDGE CASES: [4, 5, 8, 3]  -> 3 
+             [2]  ->  1
+             [8] -> 1
+             
+
+ INITIAL APPROACH:    - [
+ */
